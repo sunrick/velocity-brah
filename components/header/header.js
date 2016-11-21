3a550a1f67f4d99530e6dba7bfe5f03ab55a7e33
Rickard Sunden <rickard.sunden@outlook.com>
2016-11-20 21:57:32 -0500

    one loop around the header brah

--- components/header/header.js
+++ components/header/header.js
@@ -4,17 +4,58 @@ $(document).ready(function(){
     var parentId = "#header"
 
     var config = {
-      parent: parentId
+      parent: parentId,
+      child: parentId + " .line",
+      parentWidth: function(){ return $(this.parent).width(); },
+      parentHeight: function() { return $(this.parent).height(); },
+      childWidth: function() { return $(this.child).width(); },
+      childHeight: function() { return $(this.child).height(); }
+    }
+
+    var animation = function(){
+      $(config.child).css({
+        position: "absolute",
+        top: 0,
+        left: 0,
+        backgroundColor: "red"
+      });
+      $(config.child).velocity({ width: "10px", height: "10px" },{ easing: "linear", duration: 100 })
+      .velocity({
+        left: function(){ return config.parentWidth() - config.childHeight(); }
+      },{
+        easing: "linear",
+        duration: 2000
+      })
+      .velocity({
+        top: function() { return config.parentHeight() - config.childHeight() }
+      },{
+        easing: "linear",
+        duration: 1000
+      })
+      .velocity({
+        left: 0
+      },{
+        easing: "linear",
+        duration: 2000
+      })
+      .velocity({
+        top: 0
+      },{
+        easing: "linear",
+        duration: 1000
+      })
     }
 
     var template = function(){
       $(config.parent).append(
-        '<h1 class="black-back title"> VELOCITY BRAH </h1>'
+        '<h1 class="black-back title"> VELOCITY BRAH </h1>' +
+        '<div class="line"></div>'
       );
     }
 
     var init = function(){
       template();
+      animation();
     }
 
     init();
